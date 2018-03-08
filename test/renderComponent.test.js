18b4186aba1cc9739b178e2fb3723b9c8c14730f
Cory Gibbons <hello@corygibbons.com>
2018-03-07 18:57:39 -0800

    Update tests

--- test/renderComponent.test.js
+++
@@ -1,9 +0,0 @@
-const renderComponent = require('../src/lib/renderComponent');
-
-describe('renderComponent()', () => {
-  it('throws an error if not a valid component', () => {
-    expect(() => {
-      renderComponent('not a component');
-    }).toThrowError('Not a valid React component');
-  });
-});
